9fa79a71ba07c3eb887271b9cb18ed94e389dff5
lgraham076 <langstongraham@gmail.com>
2025-05-07 16:02:13 -0500

    Created Hello World arm64

---
+++ hello.s
@@ -0,0 +1,20 @@
+
+.data
+helloworld:
+    .asciz "Hello World\n"
+
+.text
+.global _start
+
+_start:
+    mov x0, #1
+    ldr x1, =helloworld
+    mov x2, #12
+    mov w8, #64
+    svc #0
+
+    mov x0, #0
+    mov w8, #93
+    svc #0
+
+
