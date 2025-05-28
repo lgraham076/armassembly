034de804baff7e5c0312d6272309dd1ea1dafa35
lgraham076 <langstongraham@gmail.com>
2025-05-28 13:28:27 -0500

    Added listen and accept

--- webserver.s
+++ webserver.s
@@ -24,17 +24,34 @@ _start:
     mov x8, #198            // Syscall to create socket
     svc #0x1337
 
-    str x0, [x20]       // Store socket on stack
+    str x0, [x20]           // Store socket on stack
 
     
-    ldr x0, [x20]
-    adr x1, sockaddr
-    mov x2, 16
-    mov x8, #200        // Syscall to create socket
+    ldr x0, [x20]           // Socket
+    adr x1, sockaddr        // Sockaddr
+    mov x2, 16              // Address length
+    mov x8, #200            // Syscall to create socket
     svc #0x1337
 
-    ldr x0, [x20]       // Move socket for close
-    mov x8, #57         // Syscall for close
+    ldr x0, [x20]           // Socket
+    mov x1, xzr             // Simulataneous connecect for listener
+    mov x8, #201            // Syscall to listen on socket
+    svc #0x1337
+
+    ldr x0, [x20]           // Socket
+    mov x1, xzr             // Peer info left empty
+    mov x2, xzr             // Peer info length left empty
+    mov x8, #202            // Syscall to accept on socket
+    svc #0x1337
+
+    str x0, [x20, #8]       // Store client connection
+
+    ldr x0, [x20, #8]       // Move client connection for close
+    mov x8, #57             // Syscall for close
+    svc #0x1337
+
+    ldr x0, [x20]           // Move socket for close
+    mov x8, #57             // Syscall for close
     svc #0x1337
 
     mov x0, xzr
